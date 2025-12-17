c906975821a8380b65bfcfbe6edc39708ef6d7f2
Aaryan Anand Mudvikar <aaryanm1208@gmail.com>
2025-12-17 12:22:44 +0530

    Add Candidate class with id, name, and votes

---
+++ candidate.java
@@ -0,0 +1,11 @@
+public class Candidate {
+    int id;
+    String name;
+    int votes;
+
+    public Candidate(int id, String name) {
+        this.id = id;
+        this.name = name;
+        this.votes = 0;
+    }
+}
